62a23cc51905266f118ef7e5256ccad31d79c3b4
Hitesh Garg <garg95hitesh@gmail.com>
2017-01-26 13:08:00 +0530

    message framework implemented

--- bookmarks/account/views.py
+++ bookmarks/account/views.py
@@ -1,10 +1,11 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseRedirect
 from django.contrib.auth import authenticate, login
 from .forms import LoginForm, UserRegistrationForm, UserEditForm, ProfileEditForm
 from django.contrib.auth.decorators import login_required
 from .models import Profile
-
+from django.contrib import messages
+from django.core.urlresolvers import reverse
 
 """
 def user_login(request):
@@ -53,6 +54,11 @@ def edit(request):
         if user_form.is_valid() and profile_form.is_valid():
             user_form.save()
             profile_form.save()
+            messages.success(request, 'Profile updated'\
+                                        'Successfully')
+            return HttpResponseRedirect(reverse('dashboard'))
+        else:
+            messages.error(request, 'Error updating your profile')
     else:
         user_form = UserEditForm(instance=request.user)
         profile_form = ProfileEditForm(instance=request.user.profile)
