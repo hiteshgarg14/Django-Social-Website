750f3b6e457a0da84e3fe4eaa56f54cb007d9e1e
Hitesh Garg <garg95hitesh@gmail.com>
2017-02-15 00:00:07 +0530

    Storing a ranking in Redis

--- bookmarks/images/urls.py
+++ bookmarks/images/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     url(r'^detail/(?P<id>\d+)/(?P<slug>[-\w]+)/$',views.image_detail, name='detail'),
     url(r'^like/$', views.image_like, name='like'),
     url(r'^$', views.image_list, name='list'),
+    url(r'^ranking/$', views.image_ranking, name='create'),
 ]
--- bookmarks/images/views.py
+++ bookmarks/images/views.py
@@ -39,6 +39,8 @@ def image_detail(request, id, slug):
     image = get_object_or_404(Image, id=id, slug=slug)
     # increment total image views by 1
     total_views = r.incr('image:{}:views'.format(image.id))
+    # increment image ranking by 1
+    r.zincrby('image_ranking', image.id, 1)
     """
     Using the {% with %} template tag is useful to prevent Django from evaluating QuerySets multiple times.
     """
@@ -83,3 +85,13 @@ def image_list(request):
     if request.is_ajax():
         return render(request,'images/image/list_ajax.html',{'section': 'images', 'images': images})
     return render(request,'images/image/list.html',{'section': 'images', 'images': images})
+
+@login_required
+def image_ranking(request):
+    # get image ranking dictionary
+    image_ranking = r.zrange('image_ranking',0,-1,desc=True)[:10]
+    image_ranking_ids = [int(id) for id in image_ranking]
+    # get most viewed images
+    most_viewed = list(Image.objects.filter(id__in=image_ranking_ids))
+    most_viewed.sort(key=lambda x: image_ranking_ids.index(x.id))
+    return render(request,'images/image/ranking.html',{'section': 'images','most_viewed': most_viewed})
